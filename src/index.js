9537e3aefe8da9f85e906e913d01ad8425792f14
Hnoss33 <52913599+Hnoss33@users.noreply.github.com>
2020-08-27 21:17:44 +1000

    project compleate

---
+++ src/index.js
@@ -0,0 +1,6 @@
+/* eslint-disable eol-last */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
